4543a170b81610d267417be648a8c59cbd137410
Yohannes Tsehay <ytsehay@Yohanness-iMac-Pro.local>
2018-09-20 19:26:20 -0400

    Finished the final run for PyMT. -Fixed some image-to-boundary mismath -Fixed margin issues with plots (still doesn't work for mouse 4 --> image too large error?) -Raw data for scatter plots included

---
+++ experiments/PyMT_Final/runone.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+if [ $# -ne 2 ];
+then
+	echo "Usage: Organoid_Number Organoid_Day"
+	exit 1
+fi
+
+ORGNUM=$1
+DAYNUM=$2
+
+source /Users/ytsehay/work/py_env/bin/activate
+
+DATAFOLDER='/Users/ytsehay/work/Data_Final/PyMT'
+SUBDIRNAME=mouse_${ORGNUM}_Day${DAYNUM}
+
+python /Users/ytsehay/work/project/src/ibis2d_mine.py --datafolder ${DATAFOLDER} --images ${DATAFOLDER}/Images/${SUBDIRNAME} --coords ${DATAFOLDER}/XY/${SUBDIRNAME} --outdir ${SUBDIRNAME} --invasionvsk14 y
+
+deactivate
